77662efa87d5d46de608c522bd91db4de647d13a
ASSOUNE Mike <MASSOUNE-EXT@nexity.fr>
2019-02-05 14:30:13 +0100

    c

--- ads.js
+++ ads.js
@@ -282,8 +282,8 @@ const launch = async (loopcount, loopcount2, retry) => {
         if (loopcount2 + 1 < ads.length) {
           launch(loopcount, loopcount2 + 1)
         }
-        else if (loopcount + 3 < urls.length) {
-          launch(loopcount + 3, 0)
+        else if (loopcount + 4 < urls.length) {
+          launch(loopcount + 4, 0)
         }
         else {
           console.log('Success: ' + success)
@@ -313,4 +313,5 @@ fs.remove('save', async (err) => {
   launch(0, 0)
   launch(1, 0)
   launch(2, 0)
+  launch(3, 0)
 })
