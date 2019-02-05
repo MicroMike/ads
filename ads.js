0aead9e5d567df48fbf6e5e5a4770cc4a420a169
ASSOUNE Mike <MASSOUNE-EXT@nexity.fr>
2019-02-05 17:05:10 +0100

    c

--- ads.js
+++ ads.js
@@ -304,17 +304,15 @@ const launch = async (loopcount, loopcount2, retry) => {
       success++
 
       setTimeout(async () => {
-        await adPage.close()
-      }, 1000 * 10);
-
-      setTimeout(() => {
         if (loopcount2 + 1 < ads.length) {
           launch(loopcount, loopcount2 + 1)
         }
         else if (loopcount + 5 < urls.length) {
           // launch(loopcount + 5, 0)
         }
-      }, 1000 * 5);
+
+        await adPage.close()
+      }, 1000 * 10);
 
     }
     catch (e) {
