b5c5e9ca12fdcbde5274b1b775140afc4bad84e3
ASSOUNE Mike <MASSOUNE-EXT@nexity.fr>
2019-02-27 11:58:10 +0100

    c

--- ads.js
+++ ads.js
@@ -281,7 +281,7 @@ const launch = async (retry) => {
       browser = await puppeteer.launch(params);
     }
     catch (e) {
-      console.log(e)
+      console.log('browser error')
       count--
       return
     }
